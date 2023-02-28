15cd01cb048197fe20ad8fb301f5f7c703f957a9
JiYun <jo522115@gmail.com>
2023-02-28 21:58:18 +0900

    feat: basic UI

--- App.js
+++ App.js
@@ -1,22 +1,43 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Button title="hi" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your goal!" style={styles.textInput} />
+        <Button title="Add" />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 50,
+    paddingHorizontal: 32,
   },
-  text: { margin: 16, padding: 10, borderWidth: 1, borderRadius: 10 },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    borderRadius: 5,
+    width: "80%",
+    marginRight: 8,
+    padding: 2,
+  },
+  listContainer: {
+    flex: 5.5,
+  },
 });
