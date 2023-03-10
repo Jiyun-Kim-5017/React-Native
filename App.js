142cec20fc94aa0711b7f7d110fda1ad402cfc6b
JiYun <jo522115@gmail.com>
2023-03-10 16:06:25 +0900

    feat: goal list UI

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goalList, setGoalList] = useState([]);
+
+  const inputHandler = (inputText) => {
+    setEnteredGoal(inputText);
+  };
+
+  const addGoalHandler = () => {
+    setGoalList((currentList) => [...currentList, enteredGoal]); // 새 상태가 이전 상태에 의존한다면 함수형으로 전달하는 것이 좋음
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Your goal!" style={styles.textInput} />
-        <Button title="Add" />
+        <TextInput
+          placeholder="Your goal!"
+          style={styles.textInput}
+          onChangeText={inputHandler}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View style={styles.listContainer}>
-        <Text>List of goals...</Text>
+        {goalList.map((goal, idx) => {
+          return (
+            <Text key={idx} style={styles.listItem}>
+              {goal}
+            </Text>
+          );
+        })}
       </View>
     </View>
   );
@@ -36,8 +58,15 @@ const styles = StyleSheet.create({
     width: "80%",
     marginRight: 8,
     padding: 2,
+    paddingLeft: 5,
   },
   listContainer: {
     flex: 5.5,
+  },
+  listItem: {
+    padding: 8,
+    margin: 5,
+    backgroundColor: "#EEF6FA",
+    borderRadius: 5,
   },
 });
